a760a5595d9d023e1410bf6e7f5f225119505b81
kimaniMbatiaMoringa <kimani.mbatia@student.moringaschool.com>
2023-06-12 15:01:39 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
-function scuberGreetingForFeet(){
+
+//scuberGreetingForFeet(feetTravelled);
+
+function scuberGreetingForFeet(feetTravelled){
   // Write your code here!
+  if (feetTravelled < 400 || feetTravelled ==400){
+    return 'This one is on me!';
+  }
+
+  else if (feetTravelled > 2000 && feetTravelled < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else if (feetTravelled > 2500){
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(cityLocation){
   // Write your code here!
+  if (cityLocation=='NYC'){
+    return "Ok, sounds good."
+  }
+  else{
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return'Thank you so much.'
+
+    case 'not as generous':
+      return 'Thank you.'  
+
+    default:
+      return 'Bye.'
+  }
+
 }
